42da29548d39837a70c6669f6b823ca40df4fa68
UltamateFailure <brycejordan6@gmail.com>
2021-09-09 07:42:03 -0600

    Made an Xcode project called Algorithms

---
+++ Algorithms/IntroView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Algorithms
+//
+//  Created by Jordan, Bryce on 9/9/21.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
---
+++ Algorithms/Resources/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Jordan, Bryce on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            IntroView()
+        }
+    }
+}
